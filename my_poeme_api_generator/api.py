86a5a8a3d3cb10c201cdff9dffa5573f311d37d5
Laby Damaro Camara <ldamaro98@gmail.com>
2022-04-14 01:15:16 +0200

    importation des packages

---
+++ my_poeme_api_generator/api.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from os import abort
+from urllib import response
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import request
