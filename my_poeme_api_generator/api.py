53c1f2b211d7792232be76febe3bc9d94d9543bd
Laby Damaro Camara <ldamaro98@gmail.com>
2022-04-14 01:16:14 +0200

    configuration flask

--- my_poeme_api_generator/api.py
+++ my_poeme_api_generator/api.py
@@ -5,3 +5,8 @@ from flask import Flask
 from flask import render_template
 from flask import jsonify
 from flask import request
+import json
+from flask_cors import CORS
+from poeme_gen_model import *
+
+app = Flask(__name__)
