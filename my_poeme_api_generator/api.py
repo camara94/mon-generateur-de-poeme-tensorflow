2a11b8f7f2bee75c4a3dcc9755590550a1d38c34
Laby Damaro Camara <ldamaro98@gmail.com>
2022-04-15 00:08:51 +0200

    my_poeme_api_generator/api.py

--- my_poeme_api_generator/api.py
+++ my_poeme_api_generator/api.py
@@ -5,36 +5,49 @@ from flask import Flask
 from flask import render_template
 from flask import jsonify
 from flask import request
-import json
 from flask_cors import CORS
+from flask_cors import cross_origin
+
 from poeme_gen_model import *
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route('/', methods=['GET'])
+@cross_origin()
 def accueil():
     return render_template("index.html")
 
 
 @app.route('/api/words', methods=['GET'])
-def word_indx():
+def word_index():
     return jsonify({
         'status': 'ok',
         'poeme': get_word_index()
     })
 
 
-@app.route('/api/poemes', methods=['POST'])
+@app.route('/api/poemes', methods=['GET'])
+@cross_origin()
+def resume_auto():
+    if 'tag' in request.args:
+        tag = str(request.args['tag'])
+        return generer_poeme(seed_text=tag)[len(tag):]
+    else:
+        return "Erreur: il n' ya pas de paramètre url"
+
+
+@app.route('/api/poeme', methods=['POST'])
+@cross_origin()
 def poeme():
     if not request.json or not 'tag' in request.json:
         abort(404)
     else:
         response = generer_poeme(request.json['tag'])
-
     return jsonify({
         'status': 'ok',
-        'poeme': response[len(request.json['tag'])+1:]
+        'poeme': response
     })
 
 
